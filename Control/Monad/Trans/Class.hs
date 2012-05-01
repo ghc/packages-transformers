49bff850cb765a3218f02181243519401026ca31
Ross Paterson <ross@soi.city.ac.uk>
2012-05-01 08:15:57 +0000

    3-layer example

--- Control/Monad/Trans/Class.hs
+++ Control/Monad/Trans/Class.hs
@@ -35,6 +35,9 @@ module Control.Monad.Trans.Class (
 
     -- ** Parsing and counting
     -- $example2
+
+    -- ** Interpreter monad
+    -- $example3
   ) where
 
 -- | The class of monad transformers.  Instances should satisfy the
@@ -98,7 +101,7 @@ transformers in turn:
 > runParser :: Parser a -> String -> [(a, Int)]
 > runParser p s = [(x, n) | ((x, Sum n), "") <- runStateT (runWriterT p) s]
 
-To define @item@ parser, we need to lift the @StateT@ operations through
+To define the @item@ parser, we need to lift the @StateT@ operations through
 the @WriterT@ transformers.
 
 > item :: Parser Char
@@ -120,4 +123,65 @@ We can also define a primitive using the Writer:
 > tick = tell (Sum 1)
 
 Then the parser will keep track of how many @tick@s it executes.
+-}
+
+{- $example3
+
+This example is a cut-down version of the one in
+\"Monad Transformers and Modular Interpreters\",
+by Sheng Liang, Paul Hudak and Mark Jones in /POPL'95/
+(<http://web.cecs.pdx.edu/~mpj/pubs/modinterp.html>).
+
+Suppose we want to define an interpreter that can do I\/O and has
+exceptions, an environment and a modifiable store.  We can define
+a monad that supports all these things as a stack of monad transformers:
+
+> import Control.Monad.Trans.Class
+> import Control.Monad.Trans.State
+> import qualified Control.Monad.Trans.Reader as R
+> import qualified Control.Monad.Trans.Error as E
+>
+> type InterpM = StateT Store (R.ReaderT Env (E.ErrorT Err []))
+
+for suitable types @Store@, @Env@ and @Err@.
+
+Now we would like to be able to use the operations associated with each
+of those monad transformers on @InterpM@ actions.
+Since the uppermost monad transformer of @InterpM@ is @StateT@,
+it already has the state operations @get@ and @set@.
+
+The first of the @ReaderT@ operations, @ask@, is a simple action,
+so we can lift it through @StateT@ to @InterpM@ using 'lift':
+
+> ask :: InterpM Env
+> ask = lift R.ask
+
+The other @ReaderT@ operation, @local@, has a suitable type for lifting
+using @mapStateT@:
+
+> local :: (Env -> Env) -> InterpM a -> InterpM a
+> local f = mapStateT (R.local f)
+
+We also wish to lift the operations of @ErrorT@ through both @ReaderT@
+and @StateT@.  For the operation @throwError@, we know @throwError e@ is
+and simple action, so we can lift it through the two monad transformers
+to @InterpM@ with two @lift@s:
+
+> throwError :: Err -> InterpM a
+> throwError e = lift (lift (E.throwError e))
+
+The @catchError@ operation has a more complex type, so we need to use
+the special-purpose lifting function @liftCatch@ provided by most monad
+transformers.  Here we use the @ReaderT@ version followed by the @StateT@
+version:
+
+> catchError :: InterpM a -> (Err -> InterpM a) -> InterpM a
+> catchError = liftCatch (R.liftCatch E.catchError)
+
+We could lift 'IO' actions to @InterpM@ using three @lift@s, but @InterpM@
+is automatically an instance of @MonadIO@, so we can use @liftIO@ instead:
+
+> putStr :: String -> InterpM ()
+> putStr s = liftIO (Prelude.putStr s)
+
 -}
