3e513ec80ae37e704987e39c466e72c3b0c2e0cb
Ross Paterson <ross@soi.city.ac.uk>
2012-12-07 13:46:45 +0000

    add missing file Control/Monad/Signatures.hs

---
+++ Control/Monad/Signatures.hs
@@ -0,0 +1,36 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Control.Monad.Signatures
+-- Copyright   :  (c) Ross Paterson 2012
+-- License     :  BSD-style (see the file LICENSE)
+--
+-- Maintainer  :  ross@soi.city.ac.uk
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Signatures for monad operations that require specialized lifting.
+-----------------------------------------------------------------------------
+
+module Control.Monad.Signatures (
+    CallCC, Catch, Listen, Pass, Pause
+  ) where
+
+-- | Signature of the @callCC@ operation,
+-- introduced in "Control.Monad.Trans.Cont".
+type CallCC m a b = ((a -> m a) -> m b) -> m a
+
+-- | Signature of the @catchError@ operation,
+-- introduced in "Control.Monad.Trans.Error".
+type Catch e m a = m a -> (e -> m a) -> m a
+
+-- | Signature of the @listen@ operation,
+-- introduced in "Control.Monad.Trans.Writer".
+type Listen w m a = m a -> m (a, w)
+
+-- | Signature of the @pass@ operation,
+-- introduced in "Control.Monad.Trans.Writer".
+type Pass w m a =  m (a, w -> w) -> m a
+
+-- | Signature of the @pause@ operation,
+-- introduced in "Control.Monad.Trans.Free".
+type Pause f m a = f (m a) -> m a
