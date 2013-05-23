ae5e562e9561275891423c910d6f1d740fe28403
Ross Paterson <ross@soi.city.ac.uk>
2013-05-23 16:40:50 +0000

    add delimited continuations

--- Control/Monad/Trans/Cont.hs
+++ Control/Monad/Trans/Cont.hs
@@ -10,6 +10,10 @@
 --
 -- Continuation monads.
 --
+-- Delimited continuation operators are taken from Kenichi Asai and Oleg
+-- Kiselyov's tutorial at CW 2011, \"Introduction to programming with
+-- shift and reset\" (<http://okmij.org/ftp/continuations/#tutorial>).
+--
 -----------------------------------------------------------------------------
 
 module Control.Monad.Trans.Cont (
@@ -17,13 +21,19 @@ module Control.Monad.Trans.Cont (
     Cont,
     cont,
     runCont,
+    evalCont,
     mapCont,
     withCont,
+    -- ** Delimited continuations
+    reset, shift,
     -- * The ContT monad transformer
     ContT(..),
+    evalContT,
     mapContT,
     withContT,
     callCC,
+    -- ** Delimited continuations
+    resetT, shiftT,
     -- * Lifting other operations
     liftLocal,
   ) where
@@ -46,18 +56,24 @@ The @>>=@ operator adds the bound function into the continuation chain.
 type Cont r = ContT r Identity
 
 -- | Construct a continuation-passing computation from a function.
--- (The inverse of 'runCont'.)
+-- (The inverse of 'runCont')
 cont :: ((a -> r) -> r) -> Cont r a
 cont f = ContT (\c -> Identity (f (runIdentity . c)))
 
--- | Runs a CPS computation, returns its result after applying the final
--- continuation to it.
--- (The inverse of 'cont'.)
+-- | The result of running a CPS computation with a given final continuation.
+-- (The inverse of 'cont')
 runCont :: Cont r a	-- ^ continuation computation (@Cont@).
     -> (a -> r)		-- ^ the final continuation, which produces
 			-- the final result (often 'id').
     -> r
 runCont m k = runIdentity (runContT m (Identity . k))
+
+-- | The result of running a CPS computation with the identity as the
+-- final continuation.
+--
+-- * @'evalCont' ('return' x) = x@
+evalCont :: Cont r r -> r
+evalCont m = runIdentity (evalContT m)
 
 -- | Apply a function to transform the result of a continuation-passing
 -- computation.
@@ -73,11 +89,31 @@ mapCont f = mapContT (Identity . f . runIdentity)
 withCont :: ((b -> r) -> (a -> r)) -> Cont r a -> Cont r b
 withCont f = withContT ((Identity .) . f . (runIdentity .))
 
-{- |
-The continuation monad transformer.
-Can be used to add continuation handling to other monads.
--}
+-- | @'reset' m@ delimits the continuation of any 'shift' inside @m@.
+--
+-- * @'reset' ('return' m) = 'return' m@
+--
+reset :: Cont r r -> Cont r' r
+reset = resetT
+
+-- | @'shift' f@ captures the continuation up to the nearest enclosing
+-- 'reset' and passes it to @f@:
+--
+-- * @'reset' ('shift' f >>= k) = 'reset' (f ('evalCont' . k))@
+--
+shift :: ((a -> r) -> Cont r r) -> Cont r a
+shift f = shiftT (f . (runIdentity .))
+
+-- | The continuation monad transformer.
+-- Can be used to add continuation handling to other monads.
 newtype ContT r m a = ContT { runContT :: (a -> m r) -> m r }
+
+-- | The result of running a CPS computation with 'return' as the
+-- final continuation.
+--
+-- * @'evalContT' ('lift' m) = m@
+evalContT :: Monad m => ContT r m r -> m r
+evalContT m = runContT m return
 
 -- | Apply a function to transform the result of a continuation-passing
 -- computation.
@@ -127,6 +163,21 @@ instance (MonadIO m) => MonadIO (ContT r m) where
 -- layers deep within nested computations.
 callCC :: ((a -> ContT r m b) -> ContT r m a) -> ContT r m a
 callCC f = ContT $ \c -> runContT (f (\x -> ContT $ \_ -> c x)) c
+
+-- | @'resetT' m@ delimits the continuation of any 'shiftT' inside @m@.
+--
+-- * @'resetT' ('lift' m) = 'lift' m@
+--
+resetT :: Monad m => ContT r m r -> ContT r' m r
+resetT = lift . evalContT
+
+-- | @'shiftT' f@ captures the continuation up to the nearest enclosing
+-- 'resetT' and passes it to @f@:
+--
+-- * @'resetT' ('shiftT' f >>= k) = 'resetT' (f ('evalContT' . k))@
+--
+shiftT :: Monad m => ((a -> m r) -> ContT r m r) -> ContT r m a
+shiftT f = ContT (evalContT . f)
 
 -- | @'liftLocal' ask local@ yields a @local@ function for @'ContT' r m@.
 liftLocal :: (Monad m) => m r' -> ((r' -> r') -> m r -> m r) ->
