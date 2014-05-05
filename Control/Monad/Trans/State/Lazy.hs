43015aec22afd173a80f0692683e0c5632e8d2e0
Ross Paterson <ross@soi.city.ac.uk>
2014-05-05 00:26:15 +0000

    simplify labelling example in state docs

--- Control/Monad/Trans/State/Lazy.hs
+++ Control/Monad/Trans/State/Lazy.hs
@@ -340,30 +340,20 @@ it with:\"
 
 > numberTree :: Eq a => Tree a -> State (Table a) (Tree Int)
 > numberTree Nil = return Nil
-> numberTree (Node x t1 t2)
->        =  do num <- numberNode x
->              nt1 <- numberTree t1
->              nt2 <- numberTree t2
->              return (Node num nt1 nt2)
->     where
+> numberTree (Node x t1 t2) = do
+>     num <- numberNode x
+>     nt1 <- numberTree t1
+>     nt2 <- numberTree t2
+>     return (Node num nt1 nt2)
+>   where
 >     numberNode :: Eq a => a -> State (Table a) Int
->     numberNode x
->        = do table <- get
->             (newTable, newPos) <- return (nNode x table)
->             put newTable
->             return newPos
->     nNode::  (Eq a) => a -> Table a -> (Table a, Int)
->     nNode x table
->        = case (findIndexInList (== x) table) of
->          Nothing -> (table ++ [x], length table)
->          Just i  -> (table, i)
->     findIndexInList :: (a -> Bool) -> [a] -> Maybe Int
->     findIndexInList = findIndexInListHelp 0
->     findIndexInListHelp _ _ [] = Nothing
->     findIndexInListHelp count f (h:t)
->        = if (f h)
->          then Just count
->          else findIndexInListHelp (count+1) f t
+>     numberNode x = do
+>         table <- get
+>         case elemIndex x table of
+>             Nothing -> do
+>                 put (table ++ [x])
+>                 return (length table)
+>             Just i -> return i
 
 numTree applies numberTree with an initial state:
 
@@ -372,11 +362,5 @@ numTree applies numberTree with an initial state:
 
 > testTree = Node "Zero" (Node "One" (Node "Two" Nil Nil) (Node "One" (Node "Zero" Nil Nil) Nil)) Nil
 > numTree testTree => Node 0 (Node 1 (Node 2 Nil Nil) (Node 1 (Node 0 Nil Nil) Nil)) Nil
-
-sumTree is a little helper function that does not use the State monad:
-
-> sumTree :: (Num a) => Tree a -> a
-> sumTree Nil = 0
-> sumTree (Node e t1 t2) = e + (sumTree t1) + (sumTree t2)
 
 -}
