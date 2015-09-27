5678012118841b3d2d0770377656f3565ad7be68
Ross Paterson <ross@soi.city.ac.uk>
2015-09-27 15:57:28 +0000

    documentation tweak

--- Control/Monad/Trans/Class.hs
+++ Control/Monad/Trans/Class.hs
@@ -193,9 +193,9 @@ a monad that supports all these things as a stack of monad transformers:
 
 > import Control.Monad.Trans.Class
 > import Control.Monad.Trans.State
-> import Control.Monad.IO.Class
 > import qualified Control.Monad.Trans.Reader as R
 > import qualified Control.Monad.Trans.Except as E
+> import Control.Monad.IO.Class
 >
 > type InterpM = StateT Store (R.ReaderT Env (E.ExceptT Err IO))
 
